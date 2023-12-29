64bf2bed0c0ed9a04a5458b42eedb94b9260195c
yeolchiyang <yangchi97@naver.com>
2023-12-29 21:59:16 +0900

    ㅁㄴㄴㅁ

--- Assets/Script/Yang/Skeleton.cs
+++ Assets/Script/Yang/Skeleton.cs
@@ -1,20 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Yang{
     public class Skeleton : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
 
+        private UnityEngine.AI.NavMeshAgent skeletonNav;
+        //임시, 테스트 후 삭제
+        [SerializeField] private Transform targetPoint;
+
+        private void Awake()
+        {
+            skeletonNav = GetComponent<UnityEngine.AI.NavMeshAgent>();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Start()
         {
+            skeletonNav.SetDestination(targetPoint.position);
+        }
+        private void Update()
+        {
+            //moveTo();
+        }
 
+        //임시
+        private void moveTo()
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float z = Input.GetAxisRaw("Vertical");
+            Vector3 moveDirection = new Vector3(x, 0f, z);
+            moveDirection.Normalize();
+
+            transform.Translate(moveDirection * Time.deltaTime * 5f);
         }
     }
 }

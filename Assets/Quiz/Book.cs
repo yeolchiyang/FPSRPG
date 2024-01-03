c38ab2c8fb60ca6b42e6f49690f6de3852d1ec35
DESKTOP-84K80GL\LG <sojd0316@naver.com>
2024-01-03 20:36:46 +0900

    main에 임시 커밋

---
+++ Assets/Quiz/Book.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            anim.SetTrigger("Get");
+        }
+    }
+}

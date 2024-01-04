a1d37979a958089df8e90e5605ffba295e8808f6
won <dnjsehdfbf22@gmail.com>
2024-01-04 17:58:33 +0900

    won

--- Assets/Script/won/WeaponChange.cs
+++ Assets/Script/won/WeaponChange.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WeaponChange : MonoBehaviour
 {
+    Transform tr;
+    public GameObject[] Weapon;
+    int currentWeaponNumber = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tr = GetComponent<Transform>();
+        WeaponON();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeaponNumber = 0;
+            WeaponON();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeaponNumber = 1;
+            WeaponON();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentWeaponNumber = 2;
+            WeaponON();
+        }
+    }
+    void WeaponON()
+    {
+        for (int i = 0; i < Weapon.Length; i++)
+        {
+            Weapon[i].SetActive(false);
+        }
+        Weapon[currentWeaponNumber].SetActive(true);
     }
 }

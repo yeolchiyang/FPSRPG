16c0adcf37b1cd592094ba25576365e75fb0bdd3
yeolchiyang <yangchi97@naver.com>
2024-01-10 21:50:08 +0900

    Elite_Ent 상태 2개 추가

--- Assets/Script/Yang/TreeEnt/TreeEntHitDetetion.cs
+++ Assets/Script/Yang/TreeEnt/TreeEntHitDetetion.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class TreeEntHitDetetion : MonoBehaviour
 {
-
+    public float NextHittedTime { get; set; } = 0.3f;//한번 맞은 뒤 다시 때릴 수 있는 시간
+    public float CurrentHittedTime { get; set; } = 0f;
     private LayerMask playerMask;
 
     private void OnEnable()
@@ -14,12 +15,15 @@ public class TreeEntHitDetetion : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        int collidedLayer = other.gameObject.layer;
-        int playerMaskValue = 1 << playerMask.value;
-        if ( ((1 << collidedLayer) & playerMaskValue) != 0)
+        if(CurrentHittedTime + NextHittedTime <= Time.time)
         {
-            Debug.Log("Player is hitted!");
+            int collidedLayer = other.gameObject.layer;
+            int playerMaskValue = 1 << playerMask.value;
+            if (((1 << collidedLayer) & playerMaskValue) != 0)
+            {
+                CurrentHittedTime = Time.time;
+                //player 데미지 입음을 실현시킬 수 있는 메소드를 넣을 것!!!!
+            }
         }
-
     }
 }

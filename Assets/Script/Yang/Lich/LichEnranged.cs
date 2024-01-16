f45a91581ee650fb402058858b859005cee44f3e
yeolchiyang <yangchi97@naver.com>
2024-01-16 21:49:32 +0900

    Lich 임시저장

---
+++ Assets/Script/Yang/Lich/LichEnranged.cs
@@ -0,0 +1,23 @@
+using RSG;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LichEnranged : StateMachineBehaviour
+{
+    private IState rootState;
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<TreeEntController>().IsInvulnerable = true;
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        rootState = animator.GetComponent<TreeEntController>().rootState;
+        rootState.ChangeState(TreeState.EnrangedIdle.ToString());
+        animator.GetComponent<TreeEntController>().IsInvulnerable = false;
+        Debug.Log("LichEnranged 상태를 벗어남");
+    }
+
+}
---
+++ Assets/Script/Yang/Lich/LichEnrangedAttack.cs
@@ -0,0 +1,16 @@
+using RSG;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LichEnrangedAttack : StateMachineBehaviour
+{
+    private IState rootState;
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        rootState = animator.GetComponent<LichController>().rootState;
+        rootState.ChangeState(TreeState.Walk.ToString());
+    }
+}
